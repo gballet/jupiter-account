65e4f4857efd7ae1c81211bb34c9f66ad31a8bb7
Guillaume Ballet <gballet@gmail.com>
2020-05-08 22:03:48 +0200

    And and export tx and txdata structures

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use multiproof_rs::NibbleKey;
+use multiproof_rs::{Multiproof, NibbleKey};
 
 #[derive(Debug, PartialEq)]
 pub enum Account {
@@ -69,5 +69,69 @@ impl rlp::Encodable for Account {
                     .finalize_unbounded_list();
             }
         };
+    }
+}
+
+/// Represents a layer-2 transaction.
+#[derive(Debug)]
+pub struct Tx {
+    pub from: NibbleKey,
+    pub to: NibbleKey,
+    pub nonce: u64,
+    pub value: u64,
+    pub call: u32, // Txs have only one instruction in this model, and it's a "call"
+}
+
+impl rlp::Encodable for Tx {
+    fn rlp_append(&self, stream: &mut rlp::RlpStream) {
+        stream
+            .begin_unbounded_list()
+            .append(&self.from)
+            .append(&self.to)
+            .append(&self.nonce)
+            .append(&self.value)
+            .append(&self.call)
+            .finalize_unbounded_list();
+    }
+}
+
+impl rlp::Decodable for Tx {
+    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
+        Ok(Tx {
+            from: NibbleKey::from(rlp.val_at::<Vec<u8>>(0)?),
+            to: NibbleKey::from(rlp.val_at::<Vec<u8>>(1)?),
+            nonce: rlp.val_at(2)?,
+            value: rlp.val_at(3)?,
+            call: rlp.val_at(4)?,
+        })
+    }
+}
+
+/// Represents the data that should be encoded inside a layer one `data` field.
+#[derive(Debug)]
+pub struct TxData {
+    pub proof: Multiproof,
+    pub txs: Vec<Tx>,
+    pub signature: Vec<u8>,
+}
+
+impl rlp::Encodable for TxData {
+    fn rlp_append(&self, stream: &mut rlp::RlpStream) {
+        stream
+            .begin_unbounded_list()
+            .append(&self.proof)
+            .append_list(&self.txs)
+            .append(&self.signature)
+            .finalize_unbounded_list();
+    }
+}
+
+impl rlp::Decodable for TxData {
+    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
+        Ok(TxData {
+            proof: rlp.val_at::<Multiproof>(0)?,
+            txs: rlp.list_at(1)?,
+            signature: rlp.val_at::<Vec<u8>>(2)?,
+        })
     }
 }
